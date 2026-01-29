2d3b426c58a9f817e56c4265b13adb0fe7ee37f9
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-01-29 15:23:56 +0800

    feat: fetch weather forecast by location, return javascript object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './styles.css';
 
-console.log("hello world!");
+const base_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const location = "Singapore";
+
+async function getWeatherForecast(location){
+    let response = await fetch(base_URL+location+'/?key='+process.env.VISUALCROSSING_KEY);
+    let forecast = await response.json();
+    return forecast;
+}
+
+getWeatherForecast(location).then(forecast => console.log(forecast));
