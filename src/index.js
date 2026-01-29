b9edb168560b359a48a901fabc43c35c41771163
Wei-Ting Yap <29475856+weitingyp@users.noreply.github.com>
2026-01-29 15:33:50 +0800

    feat: render current conditions in text on form submission

--- src/index.js
+++ src/index.js
@@ -9,4 +9,20 @@ async function getWeatherForecast(location){
     return forecast;
 }
 
-getWeatherForecast(location).then(forecast => console.log(forecast));
+const locationInput = document.querySelector("#location-input");
+const submitBtn = document.querySelector("#submit-btn");
+submitBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    getWeatherForecast(locationInput.value).then(renderForecast);
+})
+
+
+const forecastContainer = document.querySelector("#forecast-container");
+function renderForecast(forecast){
+    forecastContainer.innerText = `
+    Today in ${forecast.address} it is ${forecast.currentConditions.conditions}
+    Feels like: ${forecast.currentConditions.feelslike}deg Fahrenheit
+    Humidity: ${forecast.currentConditions.humidity}%
+    Precipitation: ${forecast.currentConditions.precip}mm
+    `;
+}
